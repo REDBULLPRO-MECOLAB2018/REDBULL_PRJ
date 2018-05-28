ec6ea04aed3b42930d077ead1eefdd176a785cc7
NAKAMOTO <mrmasnahd753@gmail.com>
2018-05-28 20:22:02 +0900

    build/追加

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/amsl/ros_catkin_ws/src/redbull_prj
+# Build directory: /home/amsl/ros_catkin_ws/src/redbull_prj/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
